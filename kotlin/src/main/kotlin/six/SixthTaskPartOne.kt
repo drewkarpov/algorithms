68e6e01a24254e2dea2a88f194f8269c97994917
drew <rostochek22@gmail.com>
2020-12-06 12:09:01 +0300

    task 6

---
+++ kotlin/src/main/kotlin/six/SixthTaskPartOne.kt
@@ -0,0 +1,24 @@
+package six
+
+fun main() {
+    println(checkEnteredNumberIsEqualKey(33, 7))
+}
+
+
+fun checkEnteredNumberIsEqualKey(twoCharsNumber: Int, key: Int): Boolean {
+    if (twoCharsNumber.toString().length == 2) {
+        val numbers = mapNumberToNumbersArray(twoCharsNumber)
+        if (numbers[0] + numbers[1] == key || numbers[0] == numbers[1]) {
+            return true
+        }
+    } else {
+        throw IllegalArgumentException("число twoCharsNumber должно быть двузначным")
+    }
+    return false
+}
+
+private fun mapNumberToNumbersArray(number: Int): MutableList<Int> {
+    val numbers = mutableListOf<Int>()
+    number.toString().toCharArray().forEach { numbers.add(it.toString().toInt()) }
+    return numbers
+}
--- kotlin/src/main/kotlin/three/ThirdTaskPartOne.kt
+++ kotlin/src/main/kotlin/three/ThirdTaskPartOne.kt
@@ -2,18 +2,18 @@ package three
 
 fun main() {
     val words = arrayOf("apple banana", "orange", "banana", "kiwi strawberry blueberry")
-    var preparedWords = arrayOf<String>()
+    var clearWords = arrayOf<String>()
 
     for (word in words) {
         val preparedWord = cleanWordFromSpecificSymbolsAndNumbers(word).trim()
         val separatedWord = preparedWord.split(" ")
         if (separatedWord.size > 1) {
-            separatedWord.forEach { preparedWords = preparedWords.plusElement(it) }
+            separatedWord.forEach { clearWords = clearWords.plusElement(it) }
         } else {
-            preparedWords = preparedWords.plusElement(word)
+            clearWords = clearWords.plusElement(word)
         }
     }
-    println(preparedWords.contentToString())
+    println(clearWords.contentToString())
 }
 
 private fun cleanWordFromSpecificSymbolsAndNumbers(word: String): String {
